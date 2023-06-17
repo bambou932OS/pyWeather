75b200a9656aeb9b0cdcae7bea090e9cad34c501
bambou932OS <bambou932+OS@gmail.com>
2023-06-17 22:14:38 +0900

    Change Output cityname more Visable

--- src/print_weather_info.py
+++ src/print_weather_info.py
@@ -1,10 +1,18 @@
 # Print weather information
 import pprint
 
+# 출력할때 색깔을 입히기 위한 함수
+def colorize(text:str, background_color_code:str, text_color_code:str, length:int=0) -> str:
+    if(length == 0): length = len(text) # length가 주어지지 않았으면, text의 길이로 설정
+
+    text_color = f'\u001b[38;5;256{text_color_code}m'
+    background_color = f'\u001b[48;5;256{background_color_code}m'
+    
+    return f'{background_color}{text_color}{text:^{length}}\u001b[0m'
+
 def print_weather_info(weather_data, metric=True):
     #pprint.pprint(weather_data)
-
     print(
-        f'The weather in {weather_data["name"]}({weather_data["sys"]["country"]}) '
-        f'is {weather_data["main"]["temp"]}°{"C" if metric else "F"}, {weather_data["weather"][0]["description"].capitalize()}'
+        colorize(weather_data["name"]+f'({weather_data["sys"]["country"]})', 7, 0, 15) +
+        f'    {weather_data["main"]["temp"]}°{"C" if metric else "F"}, {weather_data["weather"][0]["description"].capitalize()}'
     )
